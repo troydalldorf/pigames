b4821e641a5177b34ac9b0d2cc70182af8522cde
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-05 19:57:00 -0600

    gpio

---
+++ tests/CheckGpio/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Device.Gpio;
+
+Console.WriteLine("Hello, World!");
+var buttonPin = 27;
+using GpioController controller = new();
+controller.OpenPin(buttonPin, PinMode.InputPullUp);
+
+while (true)
+{
+    if (controller.Read(buttonPin) == PinValue.Low)
+    {
+        Console.WriteLine("Down");
+    }
+    else
+    {
+        Console.WriteLine("None");
+    }
+} 
