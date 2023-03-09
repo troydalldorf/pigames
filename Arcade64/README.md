0617a68fad426d51ade1c310c2d49d9f14f8fc70
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-08 19:47:43 -0600

    MySeeSaw

--- Arcade64/README.md
+++ Arcade64/README.md
@@ -1,7 +1,6 @@
 # Guide
 https://learn.adafruit.com/adafruit-rgb-matrix-bonnet-for-raspberry-pi/driving-matrices
 
-
 # Install .Net
 The secure shell will prompt you for the password for the user pi. Once you are connected to the Raspberry Pi, issue the following command to download and install the .NET 5 dotnet SDK and runtime.
 
@@ -36,8 +35,19 @@ https://github.com/hzeller/rpi-rgb-led-matrix/tree/master/bindings/c%23
 # GPIO
 
 # I2C
+
+### Debug
+``` bash
+sudo i2cdetect -y 1
+```
+
+### Python (Just to See if things Work)
+https://github.com/adafruit/Adafruit_CircuitPython_seesaw/blob/main/examples/seesaw_arcade_qt_multi_board.py
+
+### C#
+https://github.com/dotnet/iot
+
 install Iot.Device.Bindings
 
-# Hardware
+### Hardware
 - https://www.adafruit.com/product/5296
-- 
--- tests/CheckSeeSaw/Program.cs
+++ tests/CheckSeeSaw/Program.cs
@@ -1,16 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Device.Gpio;
 using System.Device.I2c;
-using Iot.Device.CharacterLcd;
-using Iot.Device.Mcp23xxx;
-using Iot.Device.Seesaw;
+using CheckSeeSaw;
 
 Console.WriteLine("Hello, World!");
 
-using var i2cDevice = I2cDevice.Create(new I2cConnectionSettings(1,0x3a));
-using var seeSaw = new Seesaw(i2cDevice);
+using var i2CDevice = I2cDevice.Create(new I2cConnectionSettings(1,0x3a));
+using var seeSaw = new MySeesaw(i2CDevice);
 while (true)
 {
-    Console.WriteLine($"{seeSaw.ReadGpioDigital(0)}, {seeSaw.ReadGpioDigital(1)}, {seeSaw.ReadGpioDigital(2)}, {seeSaw.ReadGpioDigital(3)}");
+    Console.WriteLine($"{seeSaw.ReadGpioDigital(18)}, {seeSaw.ReadGpioDigital(19)}, {seeSaw.ReadGpioDigital(20)}, {seeSaw.ReadGpioDigital(2)}");
 }
