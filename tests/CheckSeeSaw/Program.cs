f178dc798bfe225bc1bbfe22bb16d69bcab8c0af
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-10 13:15:03 -0600

    set pin mode

--- tests/CheckSeeSaw/Program.cs
+++ tests/CheckSeeSaw/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Device.Gpio;
 using System.Device.I2c;
 using CheckSeeSaw;
 using Iot.Device.Seesaw;
@@ -8,9 +9,13 @@ Console.WriteLine("Hello, World!");
 
 using var i2CDevice = I2cDevice.Create(new I2cConnectionSettings(1,0x3a));
 using var seeSaw = new MySeesaw(i2CDevice);
+seeSaw.SetGpioPinMode(18, PinMode.Input);
+seeSaw.SetGpioPinMode(19, PinMode.Input);
+seeSaw.SetGpioPinMode(20, PinMode.Input);
+seeSaw.SetGpioPinMode(2, PinMode.Input);
 Console.WriteLine($"version: {seeSaw.Version}");
 while (true)
 {
-    Console.WriteLine($"{seeSaw.ReadGpioDigital(1)}, {seeSaw.ReadGpioDigital(2)}, {seeSaw.ReadGpioDigital(3)}, {seeSaw.ReadGpioDigital(4)}");
+    Console.WriteLine($"{seeSaw.ReadGpioDigital(18)}, {seeSaw.ReadGpioDigital(19)}, {seeSaw.ReadGpioDigital(20)}, {seeSaw.ReadGpioDigital(2)}");
     System.Threading.Thread.Sleep(50);
 }
