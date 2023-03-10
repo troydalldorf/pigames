0bc83cdf8ac3b273adc1712fb43abf9bc2cc6021
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-10 16:46:12 -0600

    up, dn, lt, rt

--- tests/CheckSeeSaw/Program.cs
+++ tests/CheckSeeSaw/Program.cs
@@ -14,12 +14,12 @@ ulong pin20 = 1 << 20;
 ulong pin2 = 1 << 2;
 var pins = pin18 | pin19 | pin20 | pin2;
 seesaw.SetGpioPinModeBulk(pins, PinMode.InputPullUp);
-Console.WriteLine($"version: {seesaw.Version}");
 while (true)
 {
     var data = seesaw.ReadGpioDigitalBulk(0);
-    var bitStrM = Convert.ToString((long)data, 2).PadLeft(64, '0');
-    var bitStrS = Convert.ToString((long)data, 2).PadLeft(64, '0');
-    Console.WriteLine($"my: {bitStrM} | simple: {bitStrS}");
-    System.Threading.Thread.Sleep(50);
+    var up = (data & pin18) > 0;
+    var down = (data & pin19) > 0;
+    var left = (data & pin20) > 0;
+    var right = (data & pin2) > 0;
+    Console.WriteLine($"up: {up}, dn: {down}, lt: {left}, rt: {right}");
 }
