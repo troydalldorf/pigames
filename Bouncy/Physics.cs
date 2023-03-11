ae50c2e239b1f2977997ce3f14cbb2aa4ac59f13
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-10 19:28:29 -0600

    bounce more

--- Bouncy/Physics.cs
+++ Bouncy/Physics.cs
@@ -5,6 +5,7 @@ public class Physics
     public int X { get; set; }
     public int Y { get; set; }
     public int VelocityY { get; set; }
+    public int VelocityX { get; set; }
     public int Gravity { get; set; }
     public int Elasticity { get; set; }
     public int Friction { get; set; }
--- Bouncy/Program.cs
+++ Bouncy/Program.cs
@@ -5,16 +5,27 @@ using Core.Inputs;
 Console.WriteLine("Starting...");
 var player1 = new Player(0x3a);
 var display = new LedDisplay();
-var ball = new Physics { X = 32, Y = 32, VelocityY = 0, Gravity = 1, Elasticity = 2, Friction = 1 };
+var ball = new Physics { X = 32, Y = 1, VelocityY = 0, VelocityX = 1, Gravity = 1, Elasticity = 2, Friction = 1 };
 while (true)
 {
     display.Clear();
     ball.Y += ball.VelocityY;
+    ball.X += ball.VelocityX;
     ball.VelocityY += ball.Gravity;
-    if (ball.Y > 64)
+    if (ball.Y > 63)
     {
         ball.Y = 63;
         ball.VelocityY = -ball.VelocityY / ball.Elasticity - ball.Friction;
+    }
+    if (ball.X > 63)
+    {
+        ball.VelocityX = -1;
+        ball.X = 63;
+    }
+    if (ball.X < 0)
+    {
+        ball.VelocityX = +1;
+        ball.X = 0;
     }
     display.DrawCircle(ball.X, ball.Y, 1, new Color(155, 75, 155));
     display.Update();
