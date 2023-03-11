7576768165f2f593e55a61c97666d5baa230501d
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-10 19:22:24 -0600

    ball

---
+++ Bouncy/Physics.cs
@@ -0,0 +1,11 @@
+namespace Bouncy;
+
+public class Physics
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int VelocityY { get; set; }
+    public int Gravity { get; set; }
+    public int Elasticity { get; set; }
+    public int Friction { get; set; }
+}
