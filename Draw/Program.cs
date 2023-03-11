103566e6b61969b1eed3df0bc5db15adfeb13f20
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-10 18:39:29 -0600

    ethan

--- Draw/Program.cs
+++ Draw/Program.cs
@@ -4,11 +4,28 @@ using Core.Inputs;
 Console.WriteLine("Starting...");
 var player1 = new Player(0x3a);
 var display = new LedDisplay();
-
+var x = 32;
+var y = 32;
 while (true)
 {
     display.Clear();
     var js = player1.ReadJoystick();
-    display.SetPixel(32, 32, new Color(155, 0, 0));
+    if (js.IsUp())
+    {
+        y = y + 1;
+    }
+    if (js.IsDown())
+    {
+        y = y - 1;
+    }
+    if (js.IsLeft())
+    {
+        x = x - 1;
+    }
+    if (js.IsRight())
+    {
+        x = x + 1;
+    }
+    display.SetPixel(32, 32, new Color(155, 75, 155));
     display.Update();
 }
