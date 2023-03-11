d2c8fbed48a24529e6197f9f711e1e17957e029b
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-10 23:01:56 -0600

    animate

--- SpaceInvaders/Program.cs
+++ SpaceInvaders/Program.cs
@@ -10,11 +10,12 @@ var alien1C = image.GetSprite(18, 0, 8, 8);
 var alien1 = new SpriteAnimation(8, 8, alien1A, alien1B, alien1C);
 var display = new LedDisplay();
 var player = new Player(0x3a, 0x42);
-var stopwatch = new Stopwatch();
+var frame = 0;
 while (true)
 {
     display.Clear();
-    alien1.Frame = ((int)stopwatch.Elapsed.TotalMilliseconds / 250) % 2; 
+    alien1.Frame = (frame/10) % 2;
     display.DrawSprite(20, 20, alien1);
     display.Update();
+    frame++;
 }
