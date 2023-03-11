70104fd7406d63d971602170b21b5ad11171b225
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-11 09:19:19 -0600

    bouncy

--- Bouncy/Program.cs
+++ Bouncy/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Drawing;
 using Core.Display;
 
-Console.WriteLine("Starting...");
 var display = new LedDisplay();
 int x = 20;
 int y = 20;
@@ -12,6 +11,22 @@ while (true)
     display.Clear();
     x = x + dx;
     y = y + dy;
-    display.DrawCircle(x, x, 1,  Color.FromArgb(155, 75, 155));
+    display.DrawCircle(x, x, 1, Color.FromArgb(155, 75, 155));
     display.Update();
+    if(x>63)
+    {
+         dx = -1;
+    }
+    if(x<0)
+    {
+         dx = 1;
+    }
+    if(y<0)
+    {
+         dy = 1;
+    }
+    if(y>63)
+    {
+         dy = -1;
+    }
 }
