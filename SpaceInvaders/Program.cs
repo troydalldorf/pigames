daa0290c577e51c604883d6510033bb8f89df736
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-10 22:39:06 -0600

    space invaders

--- SpaceInvaders/Program.cs
+++ SpaceInvaders/Program.cs
@@ -7,8 +7,9 @@ var image = new SpriteImage("graphics.png");
 var alien = image.GetSprite(0, 0, 8, 8);
 var display = new LedDisplay();
 var player = new Player(0x3a, 0x42);
-
 while (true)
 {
+    display.Clear();
     display.DrawSprite(20, 20, alien);
+    display.Update();
 }
