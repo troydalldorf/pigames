4806217709453e45b697d90b711d04d18797aeaf
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-12 08:54:46 -0500

    bar

--- Bouncy/Program.cs
+++ Bouncy/Program.cs
@@ -8,27 +8,28 @@ int dx = 1;
 int dy = 1;
 while (true)
 {
-    display.Clear();
     x = x + dx;
     y = y + dy;
-    display.DrawCircle(x, x, 1, Color.FromArgb(155, 75, 155));
-    display.Update();
     if(x>63)
     {
-         dx = -1;
+        dx = -1;
     }
     if(x<0)
     {
-         dx = 1;
+        dx = 1;
     }
     if(y<0)
     {
-         dy = 1;
+        dy = 1;
     }
     if(y>63)
     {
-         dy = -1;
+        dy = -1;
     }
+    
+    display.Clear();
+    display.DrawCircle(x, x, 1, Color.FromArgb(0, 255, 0));
     display.DrawRectangle( 16, 60, 20, 4, Color.FromArgb(155, 155, 155));
+    display.Update();
 }
       
