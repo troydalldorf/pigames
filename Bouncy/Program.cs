7cb4aaae87b98b681911d5798149ac94301cff07
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-12 09:28:52 -0500

    bar

--- Bouncy/Program.cs
+++ Bouncy/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System.Drawing;
 using Core.Display;
+using Core.Inputs;
 
 var display = new LedDisplay();
 int x = 45;
 int y = 5;
 int dx = 1;
 int dy = 1;
+int px = 16;
+var p1Console = new PlayerConsole(0x3a, 0x42);
+
 while (true)
 {
+    var stick = p1Console.ReadJoystick();
+    if (stick.IsLeft()) px--;
+    if (stick.IsRight()) px++;
     x = x + dx;
     y = y + dy;
     if(x>63)
@@ -29,7 +36,9 @@ while (true)
     
     display.Clear();
     display.DrawRectangle(x, y, 2, 2, Color.FromArgb(0, 255, 0));
-    display.DrawRectangle(16, 60, 20, 4, Color.FromArgb(155, 155, 155));
+    display.DrawRectangle(16, 60, 20, 4, Color.FromArgb(255, 255, 255));
     display.Update();
+    
+    
 }
       
