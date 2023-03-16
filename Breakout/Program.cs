4bc5469b0738e2745e3db6dbf809c90ae4792391
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-16 00:09:10 -0500

    breakout 2

---
+++ Breakout/Program.cs
@@ -0,0 +1,10 @@
+﻿using Breakout;
+using Core.Display;
+using Core.Inputs;
+
+Console.WriteLine("Breakout game starting...");
+
+var display = new LedDisplay();
+var playerConsole = new PlayerConsole(0x3a, 0x42);
+var snakeGame = new BreakoutGame(display, playerConsole);
+snakeGame.Run();
