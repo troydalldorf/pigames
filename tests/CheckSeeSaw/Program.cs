d413b11b1faa8818d5618ed13bca4c097e663c40
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-16 18:54:51 -0500

    button test

--- tests/CheckSeeSaw/Program.cs
+++ tests/CheckSeeSaw/Program.cs
@@ -6,22 +6,29 @@ using CheckSeeSaw;
 
 Console.WriteLine("Hello, World!");
 
-using var i2CDevice = I2cDevice.Create(new I2cConnectionSettings(1,0x3a));
-using var seesaw = new MySeesaw(i2CDevice);
+using var p1Joystick = I2cDevice.Create(new I2cConnectionSettings(1,0x3a));
+using var p1Buttons = I2cDevice.Create(new I2cConnectionSettings(1,0x42));
+using var ssP1Joystick = new MySeesaw(p1Joystick);
+using var ssP1Buttons = new MySeesaw(p1Buttons);
 ulong pin18 = 1 << 18;
 ulong pin19 = 1 << 19;
 ulong pin20 = 1 << 20;
 ulong pin2 = 1 << 2;
 var pins = pin18 | pin19 | pin20 | pin2;
-seesaw.SetGpioPinModeBulk(pins, PinMode.InputPullUp);
-seesaw.SetGpioPinMode(12, PinMode.Output);
+ssP1Joystick.SetGpioPinModeBulk(pins, PinMode.InputPullUp);
+ssP1Buttons.SetGpioPinMode(12, PinMode.Output);
 while (true)
 {
-    var data = seesaw.ReadGpioDigitalBulk(0);
-    var right = (data & pin18) == 0;
-    var left = (data & pin19) == 0;
-    var down = (data & pin20) == 0;
-    var up = (data & pin2) == 0;
-    seesaw.WriteGpioDigital(12, left | right | down | up);
-    Console.WriteLine($"up: {up}, dn: {down}, lt: {left}, rt: {right}");
+    // P1 Joystick
+    var dataP1Joystick = ssP1Joystick.ReadGpioDigitalBulk(0);
+    var right = (dataP1Joystick & pin18) == 0;
+    var left = (dataP1Joystick & pin19) == 0;
+    var down = (dataP1Joystick & pin20) == 0;
+    var up = (dataP1Joystick & pin2) == 0;
+    var dataP1Buttons = ssP1Joystick.ReadGpioDigitalBulk(0);
+    var red = (dataP1Buttons & pin18) == 0;
+    var green = (dataP1Buttons & pin19) == 0;
+    var blue = (dataP1Buttons & pin20) == 0;
+    Console.WriteLine($"P1 => up: {up}, dn: {down}, lt: {left}, rt: {right} | red:{red}, green: {green}, blue: {blue}");
+    //ssP1Buttons.WriteGpioDigital(12, left | right | down | up);
 }
