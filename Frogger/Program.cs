b36122f673f1b2828fc6101803e476a52489a92a
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-16 00:15:05 -0500

    frogger 2

---
+++ Frogger/Program.cs
@@ -0,0 +1,10 @@
+﻿using Core.Display;
+using Core.Inputs;
+using Frogger;
+
+Console.WriteLine("Frogger game starting...");
+
+var display = new LedDisplay();
+var playerConsole = new PlayerConsole(0x3a, 0x42);
+var snakeGame = new FroggerGame(display, playerConsole);
+snakeGame.Run();
