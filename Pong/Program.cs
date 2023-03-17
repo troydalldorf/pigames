d9d793fb5fb22038b8a94811f64249da505f1091
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-16 19:16:31 -0500

    pong

--- Pong/Program.cs
+++ Pong/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Pong game starting...");
 
 var display = new LedDisplay();
 var player1Console = new PlayerConsole(0x3a, 0x42);
-var player2Console = new PlayerConsole(0x43, 0x44);
+var player2Console = new PlayerConsole(0x44, 0x43);
 
 var pongGame = new PongGame(display, player1Console, player2Console);
 pongGame.Run();
