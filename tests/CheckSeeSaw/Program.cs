6694b896390090de0200b746887eb49f5a493b14
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-18 13:13:12 -0500

    light up buttons

--- tests/CheckSeeSaw/Program.cs
+++ tests/CheckSeeSaw/Program.cs
@@ -14,4 +14,5 @@ while (true)
     var p2Buttons = p2Console.ReadButtons();
     Console.WriteLine($"P1 => stick: {p1Stick.ToString()} | buttons: {p1Buttons.ToString()} || P2 => stick: {p2Stick.ToString()} | buttons: {p2Buttons.ToString()}");
     p1Console.LightButtons(p1Buttons.IsRedPushed(), p1Buttons.IsGreenPushed(), p1Buttons.IsBluePushed(), false);
+    p2Console.LightButtons(p2Buttons.IsRedPushed(), p2Buttons.IsGreenPushed(), p2Buttons.IsBluePushed(), false);
 }
