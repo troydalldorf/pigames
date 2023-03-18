d6a48afa0c692c0d64a7624b984916f7d3bc74b5
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-18 13:33:40 -0500

    epong

---
+++ ePong/Program.cs
@@ -0,0 +1,12 @@
+﻿using Core.Display;
+using Core.Inputs;
+using ePong;
+
+Console.WriteLine("Pong game starting...");
+
+var display = new LedDisplay();
+var player1Console = new Player1Console();
+var player2Console = new Player2Console();
+
+var pongGame = new PongGame(display, player1Console, player2Console);
+pongGame.Run();
