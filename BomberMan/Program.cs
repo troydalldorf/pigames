1e4020cee33cbd2d913bb37b079d40cda62eba2f
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-20 22:14:14 -0500

    BM

---
+++ BomberMan/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BomberMan;
+using Core.Effects;
+
+Console.WriteLine("Starting BomberMan...");
+var runner = new GameRunner();
+runner.Run(() => new BombermanGame());
+Console.WriteLine("Exiting BomberMan...");
