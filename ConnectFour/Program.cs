5c6f072be0bee8560c9078b58b4ad8d95fc4ea80
Troy Dalldorf <tdalldorf@landolakes.com>
2023-03-24 20:37:21 -0500

    game state

--- ConnectFour/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Core.Effects;
-
-Console.WriteLine("Connect Four game starting...");
-var runner = new GameRunner();
-runner.Run(() => new ConnectFourGame());
--- Pacman/Ghost.cs
+++
@@ -1,8 +0,0 @@
-namespace Pacman;
-
-public class Ghost : PacMan
-{
-    public Ghost(int x, int y, int speed) : base(x, y, speed)
-    {
-    }
-}
