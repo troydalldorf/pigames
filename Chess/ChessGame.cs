9ea353a370bc7a5bcc4f1f94fa5059de52353eb2
Troy Dalldorf <tdalldorf@landolakes.com>
2023-04-15 13:57:18 -0500

    Chess

--- Chess/ChessGame.cs
+++ Chess/ChessGame.cs
@@ -8,14 +8,25 @@ public class ChessGame : IDuoPlayableGameElement
 {
     private SpriteAnimation whitePieces;
     private SpriteAnimation blackPieces;
-    
+    private Piece[,] board = new Piece[8, 8];
     public ChessGame()
     {
         var image = SpriteImage.FromResource("chess.png");
         whitePieces = image.GetSpriteAnimation(9, 1, 8, 8, 6, 1)
-        State = GameOverState.None;
         blackPieces = image.GetSpriteAnimation(9, 9, 8, 8, 6, 1)
         State = GameOverState.None;
+        board[0, 0] = Piece.Rook;
+        board[1, 0] = Piece.Knight;
+        board[2, 0] = Piece.Bishop;
+        board[3, 0] = Piece.Queen;
+        board[4, 0] = Piece.King;
+        board[5, 0] = Piece.Bishop;
+        board[6, 0] = Piece.Knight;
+        board[7, 0] = Piece.Rook;
+        for (int i = 0; i < 8; i++)
+        {
+            board[i, 1] = Piece.Pawn;
+        }
     }
     
     public void Update()
@@ -38,6 +49,10 @@ public class ChessGame : IDuoPlayableGameElement
                     color = Color.White;
                 }
                 display.DrawRectangle(i*8, j*8,8,8, color,color);
+                if (board[i, j] != Piece.None)
+                {
+                   whitePieces.Draw(display, i*8, j*8, (int)board[i, j] - 1);
+                }
             }
         }
     }
---
+++ Chess/Piece.cs
@@ -0,0 +1,12 @@
+namespace Chess;
+
+public enum Piece
+{
+    None = 0,
+    Queen = 1,
+    King = 2,
+    Bishop = 3,
+    Knight = 4,
+    Rook = 5,
+    Pawn = 6,
+}
