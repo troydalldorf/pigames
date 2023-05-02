5120075d32ed2959d03d365d6e6d676d3fbe2e93
Troy Dalldorf <tdalldorf@landolakes.com>
2023-05-01 20:14:19 -0500

    blitz fix

--- Blitz/BlitzGame.cs
+++ Blitz/BlitzGame.cs
@@ -10,8 +10,8 @@ namespace Blitz
         private const int DisplayHeight = 64;
         private const int DisplayWidth = 64;
         private const int Buildings = 8;
-        private const int MinBuildingHeight = 2;
-        private const int MaxBuildingHeight = 7;
+        private const int MinBuildingHeight = 1;
+        private const int MaxBuildingHeight = 6;
         private const int PlaneXSpeed = 2;
         private const int PlaneYSpeed = 2;
         private int planeX;
@@ -22,10 +22,10 @@ namespace Blitz
         private int[] buildingHeights;
         private int score;
         private GameOverState gameState;
-        private ISprite plane;
-        private ISprite bomb;
-        private ISprite building;
-        private ISprite grass;
+        private readonly ISprite plane;
+        private readonly ISprite bomb;
+        private readonly ISprite building;
+        private readonly ISprite grass;
 
         public BlitzGame()
         {
@@ -105,13 +105,19 @@ namespace Blitz
 
         public void Draw(IDisplay display)
         {
-            // Draw buildings
-            for (int i = 0; i < Buildings; i++)
+            for (var i = 0; i < DisplayWidth / grass.Width; i++)
             {
+                grass.Draw(display, i*grass.Width, DisplayHeight - grass.Height);
+            }
+
+            // Draw buildings
+            for (var i = 0; i < Buildings; i++)
+            {
+                
                 for (int y = 0; y < buildingHeights[i]; y++)
                 {
                     int buildingY = DisplayHeight - (y + 1) * building.Height;
-                    building.Draw(display, i * building.Width, buildingY);
+                    building.Draw(display, i * building.Width, buildingY, y <  buildingHeights[i] - 1 ? 0 : 1);
                 }
             }
 
@@ -121,7 +127,7 @@ namespace Blitz
             // Draw bomb
             if (bombDropped)
             {
-                bomb.Draw(display, bombX * 8, bombY * 8);
+                bomb.Draw(display, bombX * 8, bombY);
             }
         }
 
