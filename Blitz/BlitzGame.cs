2bd6ace8a0d96b819d284d75a696302679efee9b
Troy Dalldorf <tdalldorf@landolakes.com>
2023-05-01 20:01:45 -0500

    blitz fix

--- Blitz/BlitzGame.cs
+++ Blitz/BlitzGame.cs
@@ -1,127 +1,130 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using Core;
 using Core.Display.Sprites;
 
-namespace Blitz;
-
-public class BlitzGame : IPlayableGameElement
+namespace Blitz
 {
-    private const int DisplayHeight = 64;
-    private const int DisplayWidth = 64;
-    private const int Buildings = 8;
-    private const int MinBuildingHeight = 2;
-    private const int MaxBuildingHeight = 7;
-    private const int PlaneXSpeed = 2;
-    private const int PlaneYSpeed = 2;
-    private int planeX;
-    private int planeY;
-    private int bombX;
-    private int bombY;
-    private bool bombDropped;
-    private int[] buildingHeights;
-    private int score;
-    private GameOverState gameState;
-    private ISprite plane;
-    private ISprite bomb;
-    private ISprite building;
-    private ISprite grass;
-
-    public BlitzGame()
+    public class BlitzGame : IPlayableGameElement
     {
-        var image = SpriteImage.FromResource("blitz.png", new Point(63, 1));
-        this.grass = image.GetSpriteAnimation(1, 1, 4, 4, 3, 1);
-        this.bomb = image.GetSpriteAnimation(1, 6, 4, 5, 1, 1);
-        this.plane = image.GetSpriteAnimation(1, 12, 13, 5, 2, 1);
-        this.building = image.GetSpriteAnimation(1, 18, 8, 8, 3, 1);
-        Initialize();
-    }
+        private const int DisplayHeight = 64;
+        private const int DisplayWidth = 64;
+        private const int Buildings = 8;
+        private const int MinBuildingHeight = 2;
+        private const int MaxBuildingHeight = 7;
+        private const int PlaneXSpeed = 2;
+        private const int PlaneYSpeed = 2;
+        private int planeX;
+        private int planeY;
+        private int bombX;
+        private int bombY;
+        private bool bombDropped;
+        private int[] buildingHeights;
+        private int score;
+        private GameOverState gameState;
+        private ISprite plane;
+        private ISprite bomb;
+        private ISprite building;
+        private ISprite grass;
 
-    private void Initialize()
-    {
-        planeX = 0;
-        planeY = 0;
-        bombDropped = false;
-        score = 0;
-        gameState = GameOverState.None;
-
-        buildingHeights = new int[8];
-        var random = new Random();
-
-        for (var i = 0; i < Buildings ; i++)
+        public BlitzGame()
         {
-            buildingHeights[i] = random.Next(MinBuildingHeight, MaxBuildingHeight);
-        }
-    }
-
-    public void HandleInput(IPlayerConsole playerConsole)
-    {
-        var buttons = playerConsole.ReadButtons();
-
-        if (!bombDropped && buttons.HasFlag(Buttons.Green))
-        {
-            bombDropped = true;
-            bombX = (planeX+plane.Width/2) / 8;
-            bombY = planeY;
-        }
-    }
-
-    public void Update()
-    {
-        if (gameState != GameOverState.None)
-        {
-            return;
+            var image = SpriteImage.FromResource("blitz.png", new Point(63, 1));
+            this.grass = image.GetSpriteAnimation(1, 1, 4, 4, 3, 1);
+            this.bomb = image.GetSpriteAnimation(1, 6, 4, 5, 1, 1);
+            this.plane = image.GetSpriteAnimation(1, 12, 13, 5, 2, 1);
+            this.building = image.GetSpriteAnimation(1, 18, 8, 8, 3, 1);
+            Initialize();
         }
 
-        planeX++;
-
-        if (planeX >= DisplayWidth)
+        private void Initialize()
         {
-            planeX = plane.Width;
-            planeY += PlaneYSpeed;
-        }
+            planeX = 0;
+            planeY = 0;
+            bombDropped = false;
+            score = 0;
+            gameState = GameOverState.None;
 
-        if (bombDropped)
-        {
-            bombY++;
+            buildingHeights = new int[Buildings];
+            var random = new Random();
 
-            if (bombY < buildingHeights[bombX])
+            for (var i = 0; i < Buildings; i++)
             {
-                buildingHeights[bombX]--;
-                bombDropped = false;
-                score++;
+                buildingHeights[i] = random.Next(MinBuildingHeight, MaxBuildingHeight + 1);
             }
         }
 
-        if (planeY < buildingHeights[planeX])
+        public void HandleInput(IPlayerConsole playerConsole)
         {
-            gameState = GameOverState.EndOfGame;
-        }
-        else if (planeY == 0)
-        {
-            gameState = GameOverState.Player1Wins;
-        }
-    }
+            var buttons = playerConsole.ReadButtons();
 
-    public void Draw(IDisplay display)
-    {
-        // Draw buildings
-        for (int i = 0; i < Buildings; i++)
-        {
-            for (int y = 0; i < buildingHeights[i]; y++)
+            if (!bombDropped && buttons.HasFlag(Buttons.Green))
             {
-                building.Draw(display, i * 8, DisplayHeight - y * 8);
+                bombDropped = true;
+                bombX = (planeX + plane.Width / 2) / 8;
+                bombY = planeY;
             }
         }
 
-        // Draw plane
-        plane.Draw(display, planeX, planeY);
-
-        // Draw bomb
-        if (bombDropped)
+        public void Update()
         {
-            bomb.Draw(display, bombX * 8, bombY * 8);
-        }
-    }
+            if (gameState != GameOverState.None)
+            {
+                return;
+            }
 
-    public GameOverState State => gameState;
+            planeX += PlaneXSpeed;
+
+            if (planeX >= DisplayWidth - plane.Width)
+            {
+                planeX = 0;
+                planeY += PlaneYSpeed;
+            }
+
+            if (bombDropped)
+            {
+                bombY++;
+
+                if (bombY >= DisplayHeight - buildingHeights[bombX] * 8)
+                {
+                    buildingHeights[bombX]--;
+                    bombDropped = false;
+                    score++;
+                }
+            }
+
+            if (planeY >= DisplayHeight - buildingHeights[planeX / 8] * 8)
+            {
+                gameState = GameOverState.EndOfGame;
+            }
+            else if (planeY >= DisplayHeight - 8)
+            {
+                gameState = GameOverState.Player1Wins;
+            }
+        }
+
+        public void Draw(IDisplay display)
+        {
+            // Draw buildings
+            for (int i = 0; i < Buildings; i++)
+            {
+                for (int y = 0; y < buildingHeights[i]; y++)
+                {
+                    int buildingY = DisplayHeight - (y + 1) * building.Height;
+                    building.Draw(display, i * building.Width, buildingY);
+                }
+            }
+
+            // Draw plane
+            plane.Draw(display, planeX, planeY);
+
+            // Draw bomb
+            if (bombDropped)
+            {
+                bomb.Draw(display, bombX * 8, bombY * 8);
+            }
+        }
+
+        public GameOverState State => gameState;
+    }
 }
