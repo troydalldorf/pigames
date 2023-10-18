41797a00772773aa68662267a7ddc76714feeb9b
Troy Dalldorf <tdalldorf@landolakes.com>
2023-10-17 20:31:05 -0500

    Use more blocks

---
+++ Stacker/Bits/Config.cs
@@ -0,0 +1,5 @@
+using System.Drawing;
+
+namespace Stacker.Bits;
+
+public record Config(Size BlockSize, int InitialBlocks, Color Color, int DisplayWidth, int DisplayHeight);
--- Stacker/Bits/Player.cs
+++ Stacker/Bits/Player.cs
@@ -7,73 +7,85 @@ namespace Stacker.Bits;
 public class Player : IGameElement
 {
     private List<StackedBlock> _stack = new();
-    private readonly string name;
-    private readonly Size _blockSize;
-    private readonly Color _color;
-    private readonly int _displayWidth;
-    private readonly int _displayHeight;
+    private readonly Config _config;
     private int _direction = 1;
     private int _x;
     private int _y;
+    private int _blocks;
     private int _speed = 1;
     private DateTime nextInput = DateTime.Now;
 
-    public Player(string name, Size blockSize, Color color, int displayWidth, int displayHeight)
+    public Player(Config config)
     {
-        this._displayWidth = displayWidth;
-        this._displayHeight = displayHeight;
-        this._y = _displayHeight - 1 - _blockSize.Height;
+        this._config = config;
+        this._y = config.DisplayHeight - 1 - config.BlockSize.Height;
         this._x = 0;
-        this.name = name;
-        this._blockSize = blockSize;
-        this._color = color;
+        this._blocks = config.InitialBlocks;
     }
-    
+
     public bool IsDone { get; private set; }
 
     public int StackSize => _stack.Count;
 
     public void Next()
     {
+        // Debounce
         if (DateTime.Now < nextInput) return;
         nextInput = DateTime.Now.AddMilliseconds(200);
         var x = _x;
-        var width = _blockSize.Width;
+        var blocks = _blocks;
         if (_stack.Any())
         {
             var last = _stack.Last();
-            var x1 = Math.Max(last.X, _x);
-            var x2 = Math.Min(last.X + last.Width, _x + _blockSize.Width);
-            x = x1;
-            width = x2 - x1;
-            IsDone = width <= 0;
+            var blockWidth = _config.BlockSize.Width + 1;
+            var x1 = Math.Max(last.X, _x) / blockWidth;
+            var x2 = Math.Min(last.X + last.Blocks, _x + _config.BlockSize.Width) / blockWidth;
+            x = x1 * blockWidth;
+            blocks = (x2 - x1);
+            IsDone = blocks <= 0;
         }
 
         if (!IsDone)
         {
-            _stack.Add(new StackedBlock(x, width));
+            _stack.Add(new StackedBlock(x, blocks));
             _direction *= -1;
-            _x = _direction < 0 ? _displayWidth - 1 - _blockSize.Width : 0; 
-            _y -= _blockSize.Height;
+            _x = _direction < 0 ? _config.DisplayWidth - 1 - _config.BlockSize.Width * _blocks : 0;
+            _y -= _config.BlockSize.Height + 1;
         }
     }
 
     public void Update()
     {
         _x += _direction;
-        if (_x+_blockSize.Width >= _displayWidth || _x <= 0) _direction *= -1;
+        var pixelWidth = (_config.BlockSize.Width + 1) * _blocks;
+        if (_x + pixelWidth >= _config.DisplayWidth || _x <= 0) _direction *= -1;
     }
 
     public void Draw(IDisplay display)
     {
-        var y = _displayHeight - 1;
+        var y = _config.DisplayHeight - 1;
         foreach (var block in _stack)
         {
-            display.DrawRectangle(block.X, y-_blockSize.Height, block.Width, _blockSize.Height, _color, _color);
-            y -= _blockSize.Height;
+            DrawRow(display, y, block);
+            y -= _config.BlockSize.Height + 1;
         }
-        display.DrawRectangle(_x, _y-_blockSize.Height, _blockSize.Width, _blockSize.Height, _color, _color);
+
+        DrawRow(display, _y, new StackedBlock(_x, _blocks));
     }
 
-    private record StackedBlock(int X, int Width);
+    private void DrawRow(IDisplay display, int y, StackedBlock block)
+    {
+        for (var i = 0; i < block.Blocks; i++)
+        {
+            display.DrawRectangle(
+                block.X + (_config.BlockSize.Width + 1) * i,
+                y - _config.BlockSize.Height,
+                _config.BlockSize.Width,
+                _config.BlockSize.Height,
+                _config.Color,
+                _config.Color);
+        }
+    }
+
+    private record StackedBlock(int X, int Blocks);
 }
